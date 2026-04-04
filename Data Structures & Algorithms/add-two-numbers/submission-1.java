ca21af45fdd56d7c98b1508dcc6c2e5baab2dc70
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-04-03 22:40:40 -0400

    Add: add-two-numbers - submission-1

---
+++ Data Structures & Algorithms/add-two-numbers/submission-1.java
@@ -0,0 +1,91 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1 == null && l2 != null || l1 == null && l2 == null){
+            return l2;
+        }
+        
+        if(l1 != null && l2 == null){
+            return l1;
+        }
+        
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        boolean carriedOne = false;
+
+        while(l1 != null && l2 != null){
+            if(carriedOne){
+                if(l1.val + l2.val > 9){
+                    current.next = new ListNode((l1.val + l2.val + 1) % 10);
+                }
+                else{
+                    current.next = new ListNode(l1.val + l2.val + 1);
+                    carriedOne = false;
+                }
+            }
+            else{
+                if(l1.val + l2.val > 9){
+                    current.next = new ListNode((l1.val + l2.val) % 10);
+                    carriedOne = true;
+                }
+                else{
+                    current.next = new ListNode(l1.val + l2.val);
+                }
+                
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+            current = current.next;
+        }
+
+        while(l2 != null){
+            if(carriedOne){
+                if(l2.val + 1 > 9){
+                    current.next = new ListNode((1 + l2.val) % 10);
+                }
+                else{
+                    current.next = new ListNode(l2.val + 1);
+                    carriedOne = false;
+                }
+            }
+            else{
+                current.next = new ListNode(l2.val);
+            }
+            l2 = l2.next;
+            current = current.next;
+        }
+
+        while(l1 != null){
+            if(carriedOne){
+                if(l1.val + 1 > 9){
+                    current.next = new ListNode((1 + l1.val) % 10);
+                }
+                else{
+                    current.next = new ListNode(l1.val + 1);
+                    carriedOne = false;
+                }
+            }
+            else{
+                current.next = new ListNode(l1.val);
+            }
+            l1 = l1.next;
+            current = current.next;
+        }
+
+        if(carriedOne){
+            current.next = new ListNode(1);
+        }
+
+        return result.next;
+    }
+}
