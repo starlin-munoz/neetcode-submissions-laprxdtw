8cdfc8636c19b810906e2e542e6791886a4d9590
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-04-06 09:00:52 -0400

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+        int left = 0;
+        int right = n - 1;
+
+        for(int i = 0; i < n; i++){
+            if(numbers[left] + numbers[right] > target){
+                numbers[right]--;
+            }
+            else if(numbers[left] + numbers[right] < target){
+                numbers[left]++;
+            }
+            else{
+                return new int[] {left + 1, right - 1};
+            }
+        }
+        return new int[] {};
+    }
+}
