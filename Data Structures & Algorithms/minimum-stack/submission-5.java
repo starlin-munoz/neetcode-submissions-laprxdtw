e2482f8bedd70d6a3b13ebf3a867e0850364591b
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-04-08 10:50:41 -0400

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.java
@@ -0,0 +1,37 @@
+class MinStack {
+    int min;
+    Stack<Integer> s;
+    Stack<Integer> mins = new Stack<>();
+
+    public MinStack() {
+        s = new Stack<Integer>();
+    }
+    
+    public void push(int val) {
+        s.push(val);
+        if(s.size() == 1){
+            min = val;
+            mins.push(min);
+        }
+        if(val <= min){
+            mins.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(s.pop() == mins.peek()){
+            mins.pop();
+        }
+    }
+    
+    public int top() {
+        return s.peek();
+    }
+    
+    public int getMin() {
+        if(mins.isEmpty()){
+            return 0;
+        }
+        return mins.peek();
+    }
+}
