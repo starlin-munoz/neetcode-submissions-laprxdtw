bbe69121c49fbb4ff26ff6d1ce11d0dcf8fe889f
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-04-09 21:03:21 -0400

    Add: valid-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-2.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root == null  || root.right == null || root.left == null){
+            return false;
+        }
+
+        if(root.left.val < root.val && root.right.val > root.val){
+            return true;
+        }
+
+        return isValidBST(root.left) && isValidBST(root.right);
+    }
+}
