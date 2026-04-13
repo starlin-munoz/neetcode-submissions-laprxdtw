80d7faa49969cb317b299324bf0703cf77ce44bb
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-04-13 09:50:08 -0400

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length() == 1){
+            return true;
+        }
+
+        String lowerS = "";
+        for(int i = 0; i < s.length(); i++){
+            if(Character.isLetter(s.charAt(i)) || Character.isDigit(s.charAt(i))){
+                lowerS += s.toLowerCase().charAt(i);
+            }
+        }
+
+        int r = lowerS.length() - 1;
+        for(int i = 0; i < lowerS.length(); i++){
+            if(lowerS.charAt(r) != lowerS.charAt(i)){
+                return false;
+            }
+            r--;
+        }
+        return true;
+    }
+}
