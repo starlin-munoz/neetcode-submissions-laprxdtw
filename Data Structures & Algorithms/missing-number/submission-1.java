7272c20229c87f3fcf22b0b8d9e0beae98ccca45
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-04-17 22:09:15 -0400

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length; 
+        int realSum = 0;
+        int sum = 0;
+
+        for(int i = 0; i <= n; i++){
+            realSum += i;
+        }
+
+        for(int i : nums){
+            sum += i;
+        }
+
+        return realSum - sum;
+    }
+}
+
