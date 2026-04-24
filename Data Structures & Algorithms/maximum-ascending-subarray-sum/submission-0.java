81c214ee30d0b4e70cc17395e0a141b2a9593003
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-04-23 23:02:53 -0400

    Add: maximum-ascending-subarray-sum - submission-0

---
+++ Data Structures & Algorithms/maximum-ascending-subarray-sum/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int max = 0;
+        int sum = 0;
+        int prev = nums[nums.length - 1];
+        sum += prev;
+
+        for(int i = nums.length - 2; i >= 0; i--){
+            int current = nums[i];
+            if(current < prev){
+                sum += current;
+                prev = current;
+            }
+            else{
+                max = Math.max(sum, max);
+            }
+        }
+
+        return Math.max(sum, max);
+    }
+}
