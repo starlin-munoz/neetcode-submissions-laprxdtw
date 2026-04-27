34b9dbd5ba4a8665ae69ed118ce9f5156b3d1d72
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-04-27 09:23:45 -0400

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count = 0;
+
+        for(String s: details){
+            if(Integer.parseInt(s.substring(11, 13)) > 60){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
