e5dda816f64f33dfeab95efb985fc106edc7dc93
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-04-28 12:34:09 -0400

    Add: monotonic-array - submission-0

---
+++ Data Structures & Algorithms/monotonic-array/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        int n = nums.length;
+        boolean monoIncreasing = true;
+        boolean monoDecreasing = true;
+
+        if(n == 1){
+            return true;
+        }
+
+        int prev = nums[0];
+        for(int i = 1; i < n; i++){
+            int current = nums[i];
+            if(!(prev <= current)){
+                monoIncreasing = false;
+            }
+            if(!(prev >= current)){
+                monoDecreasing = false;
+            }
+            prev = current;
+        }
+
+        return monoIncreasing || monoDecreasing;
+    }
+}
