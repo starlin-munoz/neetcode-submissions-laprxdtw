c28e820554c6ca4a780e6c09f10d7d9a13b18525
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-04-28 12:06:15 -0400

    Add: maximum-number-of-balloons - submission-4

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-4.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        HashMap<Character, Integer> word = new HashMap<>();
+        word.put('b', 1);
+        word.put('a', 1);
+        word.put('l', 2);
+        word.put('o', 2);
+        word.put('n', 1);
+
+        HashMap<Character, Integer> track = new HashMap<>();
+        for(char c : text.toCharArray()){
+            if(word.containsKey(c)){
+                track.put(c, track.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        if(track.isEmpty() || track.size() != 5){
+           return 0; 
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(Map.Entry<Character, Integer> entry : track.entrySet()){
+            if(entry.getKey() == 'l' || entry.getKey() == 'o'){
+                entry.setValue(entry.getValue() / 2);
+            }
+            min = Math.min(min, entry.getValue());
+        }
+
+        return min;
+    }
+}
