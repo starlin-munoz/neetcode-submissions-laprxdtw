bc6cd3cf6742525010f8fa4043738b621bbb2fb7
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-04-29 10:10:01 -0400

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+        
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        HashMap<Integer, Integer> index = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            int num = nums[i];
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+            if(freq.get(num) > 1){
+                if(Math.abs(index.get(num) - i) <= k){
+                    return true;
+                }
+            }
+            index.put(num, i);
+        }
+
+        return false;
+    }
+}
