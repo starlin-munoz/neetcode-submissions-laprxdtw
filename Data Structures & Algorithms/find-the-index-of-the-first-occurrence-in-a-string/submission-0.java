88cc2abd99803406d46253edd881023ce62e8214
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-05-02 21:14:06 -0400

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-0

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] count = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a'] += 1;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            if(count[s.charAt(i) - 'a'] == 1){
+                return s.indexOf(s.charAt(i));
+            }
+        }
+        return -1;
+    }
+}
