b5c54a23497f226dd8be467916ba69912e3a4e37
Starlin <157161554+starlin-munoz@users.noreply.github.com>
2026-05-05 10:22:29 -0400

    Add: minimum-changes-to-make-alternating-binary-string - submission-2

---
+++ Data Structures & Algorithms/minimum-changes-to-make-alternating-binary-string/submission-2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minOperations(String s) {
+        int n = s.length();
+        int count = 0;
+        String c = "";
+        char start = s.charAt(0);
+        for(int i = 0; i < n; i++){
+            if(start == '0'){
+                if(i % 2 == 0){
+                    c += "0";
+                }
+                else{
+                    c += "1";
+                }
+            }
+            else{
+                if(i % 2 == 0){
+                    c += "1";
+                }
+                else{
+                    c += "0";
+                }
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(s.charAt(i) != c.charAt(i)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
